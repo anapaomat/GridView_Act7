62e5061aafa031bf875a148c68110cbd68f979fd
ANA_PAOLA_PEREZ_MATA <a.20308051280526@cbtis128.edu.mx>
2023-04-23 16:11:43 +0000

    m

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,157 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Flutter App Learning',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        home: MyHomePage());
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+        appBar: AppBar(
+          title: Text("Flutter GridView"),
         ),
-      ),
-    );
+        body: GridView.count(
+          crossAxisCount: 2,
+          children: [
+            Container(
+              color: Colors.green,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image(
+                      image: AssetImage('assets/images/pastel1.jpg'),
+                      width: 80),
+                  Text("Home", style: TextStyle(color: Colors.white))
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.green,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image(
+                      image: AssetImage('assets/images/pastel2.png'),
+                      width: 60),
+                  Text("Account", style: TextStyle(color: Colors.white))
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.green,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset("assets/images/pastel3.jpg"),
+                  Text("Messages", style: TextStyle(color: Colors.white))
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.green,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset("assets/images/pastel4.jpg"),
+                  Text("Orders", style: TextStyle(color: Colors.white))
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.green,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset("assets/images/pastel5.jpg"),
+                  Text("Orders", style: TextStyle(color: Colors.white))
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.green,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset("assets/images/pastel6.jpg"),
+                  Text("Orders", style: TextStyle(color: Colors.white))
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.green,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset("assets/images/pastel7.jpg"),
+                  Text("Orders", style: TextStyle(color: Colors.white))
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.green,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset("assets/images/pastel8.jpg"),
+                  Text("Orders", style: TextStyle(color: Colors.white))
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.green,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset("assets/images/pastel9.jpg"),
+                  Text("Orders", style: TextStyle(color: Colors.white))
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.green,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset("assets/images/pastel10.jpg"),
+                  Text("Orders", style: TextStyle(color: Colors.white))
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.green,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset("assets/images/pastel11.jpg"),
+                  Text("Orders", style: TextStyle(color: Colors.white))
+                ],
+              ),
+            ),
+          ],
+          padding: EdgeInsets.all(10),
+          shrinkWrap: true,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+        ));
   }
 }
